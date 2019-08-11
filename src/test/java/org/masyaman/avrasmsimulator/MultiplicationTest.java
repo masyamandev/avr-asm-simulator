f3259c92e46b7b3b7b9cbbdfa8c5f058c797e12c
Aleksandr Maksymenko <amaksymenko@cloudmade.com>
2019-08-12 00:20:48 +0300

    Parametrized tests

--- src/test/java/org/masyaman/avrasmsimulator/MultiplicationTest.java
+++ src/test/java/org/masyaman/avrasmsimulator/MultiplicationTest.java
@@ -1,13 +1,26 @@
 package org.masyaman.avrasmsimulator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MultiplicationTest {
 
-    @Test
-    public void testUnsignedMultiplicationSizeOptimized() {
+    private static Stream<Arguments> provideTwoValues() {
+        return IntStream.range(0, 256)
+                .mapToObj(a -> Arguments.of(a, IntStream.range(0, 256)));
+    }
+
+    @MethodSource("provideTwoValues")
+    @ParameterizedTest
+    public void testUnsignedMultiplicationSizeOptimized(int uMultiplicand, IntStream uMultipliers) {
         AsmParser asmParser = new AsmParser();
         Program program = asmParser.parse( "" +
                     ".def  mc8u  =r16    ;multiplicand\n" +
@@ -31,71 +44,103 @@ class MultiplicationTest {
                     "brne  m8u_1    ;if not done, loop more"
         );
 
-        int steps = 0;
-        int count = 0;
+        AtomicInteger steps = new AtomicInteger();
+        AtomicInteger count = new AtomicInteger();
 
-        for (int i = 0; i < 256; i++) {
-            for (int j = 0; j < 256; j++) {
-                count++;
+        uMultipliers.forEach(uMultiplier -> {
+            count.incrementAndGet();
 
-                State state = new State();
-                state.setRegister("r16", i);
-                state.setRegister("r17", j);
+            State state = new State();
+            state.setRegister("r16", uMultiplicand);
+            state.setRegister("r17", uMultiplier);
 
-                steps += program.execute(state);
+            steps.addAndGet(program.execute(state));
 
-                assertEquals((i * j) & 0xFF, state.getRegister("r17"));
-                assertEquals((i * j >> 8) & 0xFF, state.getRegister("r18"));
-            }
-        }
+            assertEquals((uMultiplicand * uMultiplier) & 0xFF, state.getRegister("r17"));
+            assertEquals((uMultiplicand * uMultiplier >> 8) & 0xFF, state.getRegister("r18"));
 
-        System.out.println("Average steps = " + (steps * 1.0 / count));
+        });
+
+        System.out.println("Average steps = " + (steps.doubleValue() / count.doubleValue()));
     }
 
-    @Test
-    public void testUnsignedMultiplicationSpeedOptimized() {
+
+    @MethodSource("provideTwoValues")
+    @ParameterizedTest
+    public void testUnsignedMultiplicationSpeedOptimized(int uMultiplicand, IntStream uMultipliers) {
         AsmParser asmParser = new AsmParser();
         Program program = asmParser.parse( "" +
-                    ".def  mc8u  =r16    ;multiplicand\n" +
-                    ".def  mp8u  =r17    ;multiplier\n" +
-                    ".def  m8uL  =r17    ;result Low byte\n" +
-                    ".def  m8uH  =r18    ;result High byte\n" +
-                    ".def  mcnt8u  =r19    ;loop counter\n" +
-                    "\n" +
-                    ";***** Code\n" +
-                    "\n" +
-                    "\n" +
-                    "mpy8u:  clr  m8uH    ;clear result High byte\n" +
-                    "ldi  mcnt8u,8  ;init loop counter\n" +
-                    "lsr  mp8u    ;rotate multiplier\n" +
-                    "\n" +
-                    "m8u_1:  brcc  m8u_2    ;carry set\n" +
-                    "add   m8uH,mc8u  ;   add multiplicand to result High byte\n" +
-                    "m8u_2:  ror  m8uH    ;rotate right result High byte\n" +
-                    "ror  m8uL    ;rotate right result L byte and multiplier\n" +
-                    "dec  mcnt8u    ;decrement loop counter\n" +
-                    "brne  m8u_1    ;if not done, loop more"
+                "clr %B[result]" +
+                "mov %A[result], %[muls]" +
+                "sbrc %A[result], 7" +
+                "neg %A[result]" +
+
+                "lsr %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                "brcc .+2" +
+                "add %B[result], %[mulu]" +
+                "ror %B[result]" +
+                "ror %A[result]" +
+
+                // Bit7 is 0. This optimized block will cause issue on muls = -128. To avoid this use block similar to above.
+                "lsr %B[result]" +
+                "ror %A[result]" +
+
+                // Invert result sign
+                "tst %[muls]" +
+                "brlt .+6" +
+                "com %B[result]" +
+                "neg %A[result]" +
+                "sbci %B[result], 255"
         );
 
-        int steps = 0;
-        int count = 0;
+        AtomicInteger steps = new AtomicInteger();
+        AtomicInteger count = new AtomicInteger();
 
-        for (int i = 0; i < 256; i++) {
-            for (int j = 0; j < 256; j++) {
-                count++;
+        uMultipliers.forEach(uMultiplier -> {
+            count.incrementAndGet();
 
-                State state = new State();
-                state.setRegister("r16", i);
-                state.setRegister("r17", j);
+            State state = new State();
+            state.setRegister("r16", uMultiplicand);
+            state.setRegister("r17", uMultiplier);
 
-                steps += program.execute(state);
+            steps.addAndGet(program.execute(state));
 
-                assertEquals((i * j) & 0xFF, state.getRegister("r17"));
-                assertEquals((i * j >> 8) & 0xFF, state.getRegister("r18"));
-            }
-        }
+            assertEquals((uMultiplicand * uMultiplier) & 0xFF, state.getRegister("r17"));
+            assertEquals((uMultiplicand * uMultiplier >> 8) & 0xFF, state.getRegister("r18"));
 
-        System.out.println("Average steps = " + (steps * 1.0 / count));
+        });
+
+        System.out.println("Average steps = " + (steps.doubleValue() / count.doubleValue()));
     }
-
 }
